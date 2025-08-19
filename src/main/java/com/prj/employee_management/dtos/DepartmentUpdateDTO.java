0a8b93eb3b6b21c828f64f141917fcf786ba3182
aymaneiml <aymaneimlihi@gmail.com>
2025-08-19 21:45:23 +0200

    modification sur department + DTO

---
+++ src/main/java/com/prj/employee_management/dtos/DepartmentUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.prj.employee_management.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record DepartmentUpdateDTO(
+    
+    @NotNull(message = "name is required")
+    @Size(min = 2, max = 50, message = "min is 2 character and max is 50 character")
+    String name
+) {
+}
