f62db7696a38492bcd927c4b34eff32a6e240e56
aymaneiml <aymaneimlihi@gmail.com>
2025-08-22 10:15:59 +0200

    jwt auth 2

---
+++ src/main/java/com/prj/employee_management/controllers/AuthController.java
@@ -0,0 +1,41 @@
+package com.prj.employee_management.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prj.employee_management.dtos.LoginRequestDTO;
+import com.prj.employee_management.dtos.SignupRequestDTO;
+import com.prj.employee_management.services.AuthService;
+import com.prj.employee_management.shared.GlobalResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<GlobalResponse<String>> signup(
+        @RequestBody SignupRequestDTO signupRequest
+    ) {
+        authService.signup(signupRequest);
+        System.out.println(signupRequest.password());
+        return new ResponseEntity<>(new GlobalResponse<>("Signed UP"), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<GlobalResponse<String>> login(
+        @RequestBody LoginRequestDTO loginRequest
+    ) {
+        String token = authService.login(loginRequest);
+        return new ResponseEntity<>(new GlobalResponse<>(token), HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/com/prj/employee_management/services/AuthService.java
@@ -0,0 +1,77 @@
+package com.prj.employee_management.services;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.prj.employee_management.dtos.LoginRequestDTO;
+import com.prj.employee_management.dtos.SignupRequestDTO;
+import com.prj.employee_management.entities.Employee;
+import com.prj.employee_management.entities.UserAccount;
+import com.prj.employee_management.repositories.EmployeeRepo;
+import com.prj.employee_management.repositories.UserAccountRepo;
+import com.prj.employee_management.shared.CustomResponseException;
+import com.prj.employee_management.utils.JwtHelper;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserAccountRepo userAccountRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Autowired 
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    public void signup(SignupRequestDTO signupRequest) {
+
+        UserAccount account = new UserAccount();
+
+        Employee employee = employeeRepo.findById(signupRequest.employeeId())
+            .orElseThrow(()-> CustomResponseException.ResourceNotFound("Employee with id " +signupRequest.employeeId() + "not found !"));
+
+        account.setUsername(signupRequest.username());
+        account.setPassword(passwordEncoder.encode(signupRequest.password()));
+        account.setEmployee(employee);
+
+        userAccountRepo.save(account);
+    }
+
+    //retourn String car retourn le Token
+    public String login(LoginRequestDTO loginRequest) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.username(),
+                loginRequest.password()
+            )
+        );
+
+        UserAccount user = userAccountRepo.findOneByUsername(loginRequest.username())
+            .orElseThrow(() -> CustomResponseException.BadCredentials());
+
+        Map<String, Object> customClaims = new HashMap<>();
+        //dans cette map on peut ajouter tous les info qu'on veut
+        customClaims.put("userId", user.getId());
+        return jwtHelper.generateToken(customClaims, user);
+    }
+
+}
